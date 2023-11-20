dbd0c745d19eed16d7d0906b0e1f91d52cef0d1e
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2023-11-20 15:42:44 +0000

    arreglo del login

---
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/LoginApp.java
@@ -0,0 +1,23 @@
+package com.project.mazmorrita_project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApp.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.project.mazmorrita_project.controllers;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+
+    public void iniciarSesion(ActionEvent actionEvent) {
+    }
+
+    public void registrarse(ActionEvent actionEvent) {
+    }
+}
