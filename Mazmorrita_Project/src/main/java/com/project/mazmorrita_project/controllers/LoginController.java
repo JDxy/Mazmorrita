a7d14958dc13c37202c3d1ff953c1b2afbba9def
LeonardoJesusFiguVal <leonardojFigueroaval@gmail.com>
2023-11-21 19:16:40 +0000

    This time I hope It work

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/LoginController.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/LoginController.java
@@ -1,12 +1,46 @@
 package com.project.mazmorrita_project.controllers;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
-    public void iniciarSesion(ActionEvent actionEvent) {
+    @FXML
+    public Label MazmorritaTitle;
+
+    public void iniciarSesion(MouseEvent actionEvent) {
+        try {
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/com/project/mazmorrita_project/IniciarSesion.fxml")));
+
+            Stage window= (Stage) MazmorritaTitle.getScene().getWindow();
+
+            window.setTitle("");
+            window.setScene(scene);
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void registrarse(ActionEvent actionEvent) {
+    public void registrarse(MouseEvent actionEvent) {
+        try {
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/com/project/mazmorrita_project/Registro.fxml")));
+
+            Stage window= (Stage) MazmorritaTitle.getScene().getWindow();
+
+            window.setTitle("");
+            window.setScene(scene);
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
