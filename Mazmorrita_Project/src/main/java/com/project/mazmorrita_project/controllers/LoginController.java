70b5b4235b2c4f36ced95ff305755c202b34eaee
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-21 19:01:09 +0000

    FechasPuestas

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/LoginController.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/LoginController.java
@@ -1,8 +1,12 @@
 package com.project.mazmorrita_project.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class LoginController {
+    @FXML
+    public Label mazmorritaTittle;
 
     public void iniciarSesion(MouseEvent actionEvent) {
     }
