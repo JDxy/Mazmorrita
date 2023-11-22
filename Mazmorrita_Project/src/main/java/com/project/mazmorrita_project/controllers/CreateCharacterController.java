95d4a7193549ee8e9b3614b71b23036a85822bcb
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-22 20:52:56 +0000

    cambios en createCharacter
    añadidos controlador y atributos y metodos a la vista

---
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CreateCharacterController.java
@@ -0,0 +1,22 @@
+package com.project.mazmorrita_project.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class CreateCharacterController {
+
+    public Label crearPersonajeTitle;
+    public TextField tFNombre;
+    public Label labelDefensa;
+    public Label labelFuerza;
+    public Label labelVida;
+    public Label labelMagia;
+    public Label labelMana;
+
+    public void cancelar(MouseEvent mouseEvent) {
+    }
+
+    public void crearPersonaje(MouseEvent mouseEvent) {
+    }
+}
