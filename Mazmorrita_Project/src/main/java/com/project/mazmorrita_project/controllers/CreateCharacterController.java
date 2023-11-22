4a21bf5d316107437a026c05a994c05c83aa636d
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-22 20:55:07 +0000

    Update CreateCharacterController.java

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CreateCharacterController.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CreateCharacterController.java
@@ -1,17 +1,24 @@
 package com.project.mazmorrita_project.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class CreateCharacterController {
-
+    @FXML
     public Label crearPersonajeTitle;
+    @FXML
     public TextField tFNombre;
+    @FXML
     public Label labelDefensa;
+    @FXML
     public Label labelFuerza;
+    @FXML
     public Label labelVida;
+    @FXML
     public Label labelMagia;
+    @FXML
     public Label labelMana;
 
     public void cancelar(MouseEvent mouseEvent) {
