1f49f0e1119620634a49fa7bc8de9a1ce12bead9
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-24 12:10:31 +0100

    armery view y controller creads

---
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/ArmeryController.java
@@ -0,0 +1,41 @@
+package com.project.mazmorrita_project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class ArmeryController {
+    @FXML
+    public Label armeriaTitle;
+    @FXML
+    public Label labelDefensa;
+    @FXML
+    public Label labelFuerza;
+    @FXML
+    public Label labelVida;
+    @FXML
+    public Label labelMagia;
+    @FXML
+    public Label labelMana;
+
+    public void cancelar(MouseEvent mouseEvent) {
+    }
+
+    public void guardarCambios(MouseEvent mouseEvent) {
+    }
+
+    public void addMana(MouseEvent mouseEvent) {
+    }
+
+    public void addMagia(MouseEvent mouseEvent) {
+    }
+
+    public void addVida(MouseEvent mouseEvent) {
+    }
+
+    public void addFuerza(MouseEvent mouseEvent) {
+    }
+
+    public void addDefensa(MouseEvent mouseEvent) {
+    }
+}
