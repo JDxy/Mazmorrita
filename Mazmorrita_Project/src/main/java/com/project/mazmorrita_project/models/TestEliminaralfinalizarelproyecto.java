9f6824ab276c512606118b321820ff6bfd6c5539
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2023-11-25 00:38:29 +0100

    Test

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/TestEliminaralfinalizarelproyecto.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/TestEliminaralfinalizarelproyecto.java
@@ -1,6 +1,7 @@
 package com.project.mazmorrita_project.models;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.project.mazmorrita_project.models.LocalConnection.ExecuteChangesSql;
@@ -9,6 +10,21 @@ import static com.project.mazmorrita_project.models.SingInModel.findUser;
 
 public class TestEliminaralfinalizarelproyecto {
     public static void main(String[] args)  {
+
+        String[] listValues = new String[2];
+        listValues[0] = "dd";
+        listValues[1] = "dd";
+
+        List<HashMap<String, String>> sql = ExecuteSelectSql("SELECT id, nombre, contraseña FROM usuarios WHERE nombre = ? AND contraseña = ?", listValues);
+
+        for (HashMap<String, String> row : sql) {
+            for (Map.Entry<String, String> entry : row.entrySet()) {
+                String columnName = entry.getKey();
+                String value = entry.getValue();
+                System.out.println(columnName + ": " + value);
+            }
+            System.out.println("---"); // Separador entre filas
+        }
 
         if(findUser("dddd","contraseña")){
             System.out.println("Todo chido");
