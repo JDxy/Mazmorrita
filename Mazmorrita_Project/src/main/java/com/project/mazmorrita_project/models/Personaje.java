6d4f1bc0818289d758d204e31965ef681aeefa5a
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-25 01:17:56 +0100

    .

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/Personaje.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/Personaje.java
@@ -12,7 +12,9 @@ public class Personaje {
     private int magia;
     private int mana;
     private int experiencia;
-
+    private enum Clase  {Mago, Barbaro, Picaro};
+    private Clase clase;
+    private int idPiso;
     public Personaje(String nombre, Image avatar, int vida, int fuerza, int defensa, int magia, int mana) {
         this.nombre = nombre;
         //idUsuario=0;¿?
@@ -23,59 +25,95 @@ public class Personaje {
         this.magia = magia;
         this.mana = mana;
         experiencia=0;
+        this.clase= Clase.Mago;
+        this.idPiso=1;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public int getIdUsuario() {
         return idUsuario;
     }
+
     public Image getAvatar() {
         return avatar;
     }
+
     public int getVida() {
         return vida;
     }
+
     public int getFuerza() {
         return fuerza;
     }
+
     public int getDefensa() {
         return defensa;
     }
+
     public int getMagia() {
         return magia;
     }
+
     public int getMana() {
         return mana;
     }
+
     public int getExperiencia() {
         return experiencia;
     }
+
+    public Clase getClase() {
+        return clase;
+    }
+
+    public int getIdPiso() {
+        return idPiso;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
+
     public void setAvatar(Image avatar) {
         this.avatar = avatar;
     }
+
     public void setVida(int vida) {
         this.vida = vida;
     }
+
     public void setFuerza(int fuerza) {
         this.fuerza = fuerza;
     }
+
     public void setDefensa(int defensa) {
         this.defensa = defensa;
     }
+
     public void setMagia(int magia) {
         this.magia = magia;
     }
+
     public void setMana(int mana) {
         this.mana = mana;
     }
+
     public void setExperiencia(int experiencia) {
         this.experiencia = experiencia;
+    }
+
+    public void setClase(Clase clase) {
+        this.clase = clase;
+    }
+
+    public void setIdPiso(int idPiso) {
+        this.idPiso = idPiso;
     }
 }
