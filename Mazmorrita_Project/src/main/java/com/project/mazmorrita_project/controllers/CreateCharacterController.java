defc45e62e2f95185e1da798bddeac98d592bdcc
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-25 11:11:14 +0100

    .

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CreateCharacterController.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CreateCharacterController.java
@@ -1,5 +1,6 @@
 package com.project.mazmorrita_project.controllers;
 
+import com.project.mazmorrita_project.models.Alert;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -24,6 +25,7 @@ public class CreateCharacterController {
     public Label labelMagia;
     @FXML
     public Label labelMana;
+    private String clase=null;
 
     public void cancelar(javafx.scene.input.MouseEvent mouseEvent) {
         try {
@@ -36,22 +38,44 @@ public class CreateCharacterController {
             throw new RuntimeException(e);
         }
     }
-
     public void comenzar(MouseEvent mouseEvent) {
-        try {
-            Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/com/project/mazmorrita_project/floor.fxml")));
-            Stage window= (Stage) crearPersonajeTitle.getScene().getWindow();
-            window.setScene(scene);
-            window.setTitle("floor");
-            window.show();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+        if (tFNombre.getText().isEmpty()) {
+            Alert.showAlert("Error", "Debe insertar un nombre para el personaje", javafx.scene.control.Alert.AlertType.ERROR);
+        } else if (clase == null) {
+            Alert.showAlert("Error", "Debe seleccionar una clase", javafx.scene.control.Alert.AlertType.ERROR);
+        } else {
+            //crear personaje y mover a piso
+            try {
+                Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/com/project/mazmorrita_project/floor.fxml")));
+                Stage window = (Stage) crearPersonajeTitle.getScene().getWindow();
+                window.setScene(scene);
+                window.setTitle("floor");
+                window.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
     public void claseMago(MouseEvent mouseEvent) {
+        labelFuerza.setText("5");
+        labelDefensa.setText("5");
+        labelMagia.setText("10");
+        labelMana.setText("10");
+        clase="Mago";
     }
     public void claseBarbaro(MouseEvent mouseEvent) {
+        labelFuerza.setText("10");
+        labelDefensa.setText("10");
+        labelMagia.setText("0");
+        labelMana.setText("0");
+        clase="Barbaro";
     }
     public void clasePicaro(MouseEvent mouseEvent) {
+        labelFuerza.setText("20");
+        labelDefensa.setText("5");
+        labelMagia.setText("0");
+        labelMana.setText("0");
+        clase="Picaro";
     }
 }
