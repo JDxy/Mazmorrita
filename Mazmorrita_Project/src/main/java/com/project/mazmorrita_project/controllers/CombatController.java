4588e00cfd3e1be074a75ab96b5471c5c1dfc7e3
AIPONCEP <ana_ponce_perez@hotmail.es>
2023-11-25 11:20:27 +0100

    .

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CombatController.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/controllers/CombatController.java
@@ -1,9 +1,14 @@
 package com.project.mazmorrita_project.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CombatController {
     @FXML
@@ -12,6 +17,15 @@ public class CombatController {
     public ComboBox elegirAtaque;
 
     public void salir(MouseEvent mouseEvent) {
+        try {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/com/project/mazmorrita_project/floor.fxml")));
+        Stage window = (Stage) combatTitle.getScene().getWindow();
+        window.setScene(scene);
+        window.setTitle("floor");
+        window.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void luchar(MouseEvent mouseEvent) {
