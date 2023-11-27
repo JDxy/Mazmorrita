2021b6455479d09250102d884f501afd8d2a3dbe
Jose David Falcon Sosa <90795629+JDxy@users.noreply.github.com>
2023-11-27 01:14:45 +0100

    ANA , CAMBIA TU CONTRASEÑA

--- Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/LocalConnection.java
+++ Mazmorrita_Project/src/main/java/com/project/mazmorrita_project/models/LocalConnection.java
@@ -8,7 +8,8 @@ import java.util.List;
 public class LocalConnection {
     private static final String url = "jdbc:mysql://localhost:3306/proyectomazmorrita";
     private static final String user = "root";
-    private static final String password = "12345";
+    // ANA, DEJA LA CONTRASEÑA COMO ESTA, CAMBIA LA DE TU CONEXION!!!
+    private static final String password = "1234";
     private static Connection connect = null;
 
     public static Connection getConnection() {
